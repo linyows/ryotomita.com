62d63523532a317ecce6f02304d637a6208022e7
linyows <linyows@gmail.com>
2023-02-19 22:34:07 +0900

    update design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Noto_Sans_JP } from '@next/font/google'
+
+const noto = Noto_Sans_JP({ weight: ['400', '900'], subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="main">
+    <div className={`${noto.className} main notionate-override-callout`}>
       <Component {...pageProps} />
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,83 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Noto_Sans_JP } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { FetchBlocks, FetchPage, GetPageResponseEx, ListBlockChildrenResponseEx } from 'notionate'
+import { FetchBlocks, FetchPage, FetchDatabase, GetPageResponseEx, ListBlockChildrenResponseEx, QueryDatabaseParameters, PageObjectResponse } from 'notionate'
 import { Blocks } from 'notionate/dist/components'
 import 'notionate/dist/styles/notionate.css'
 
-const noto = Noto_Sans_JP({ weight: ['400', '900'], subsets: ['latin'] })
-
-type Props = {
+type TitleAndBlocks = {
+  title: string
   page: GetPageResponseEx
-  home: ListBlockChildrenResponseEx
-  manifesto: ListBlockChildrenResponseEx
-  archivement: ListBlockChildrenResponseEx
+  blocks: ListBlockChildrenResponseEx 
 }
 
+type Evaluation = {
+  name: string
+  unit: string
+  number: number
+}
+
+type Props = {
+  message: TitleAndBlocks
+  manifesto: TitleAndBlocks
+  archivement: TitleAndBlocks
+  profile: TitleAndBlocks
+  vision: TitleAndBlocks
+  collaboration: Evaluation
+  reward: Evaluation
+}
+
+const buildTitleAndBlocks = async (id: string): Promise<TitleAndBlocks> => {
+  const page = await FetchPage(id)
+  // @ts-ignore
+  const title = page.properties.title.title[0].plain_text
+  const blocks = await FetchBlocks(id)
+  return {
+    title,
+    page,
+    blocks,
+  }
+}
+
+const getEvaluation = async (slug: string): Promise<Evaluation> => {
+  const { results } = await FetchDatabase({ database_id: evaluationid })
+  const props = results.find(v => {
+    // @ts-ignore
+    return v.properties.Slug.select.name === slug
+  }) as PageObjectResponse
+  return {
+    // @ts-ignore
+    name: props.properties.Name.title[0].plain_text,
+    // @ts-ignore
+    unit: props.properties.Unit.rich_text[0].plain_text,
+    // @ts-ignore
+    number: props.properties.Number.number,
+  }
+}
+
+const messageid = process.env.NOTION_HOME_ID as string
+const manifestoid = process.env.NOTION_MANIFESTO_ID as string
+const archivementid = process.env.NOTION_ARCHIVEMENT_ID as string
+const profileid = process.env.NOTION_PROFILE_ID as string
+const evaluationid = process.env.NOTION_EVALUATION_ID as string
+const visionid = process.env.NOTION_VISION_ID as string
+
 export const getStaticProps: GetStaticProps = async (context) => {
-  const page = await FetchPage(process.env.NOTION_HOME_ID as string)
-  const home = await FetchBlocks(process.env.NOTION_HOME_ID as string)
-  const manifesto = await FetchBlocks(process.env.NOTION_MANIFESTO_ID as string)
-  const archivement = await FetchBlocks(process.env.NOTION_ARCHIVEMENT_ID as string)
   return {
     props: {
-      page,
-      home,
-      manifesto,
-      archivement,
+      message: await buildTitleAndBlocks(messageid),
+      manifesto: await buildTitleAndBlocks(manifestoid),
+      archivement: await buildTitleAndBlocks(archivementid),
+      profile: await buildTitleAndBlocks(profileid),
+      vision: await buildTitleAndBlocks(visionid),
+      collaboration: await getEvaluation('collaboration'),
+      reward: await getEvaluation('reward'),
     }
   }
 }
 
-const Home: NextPage<Props> = ({ page, home, manifesto, archivement }) => {
+const Home: NextPage<Props> = ({ message, manifesto, archivement, profile, vision, collaboration, reward }) => {
   return (
     <>
       <Head>
@@ -41,45 +87,80 @@ const Home: NextPage<Props> = ({ page, home, manifesto, archivement }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={noto.className}>
-        <div className={styles.main}>
-          <Image className={styles.portrait} src={page.icon.src} alt="ryo tomita" width={300} height={450} priority />
+      <main>
+        <div className={styles.header}>
           <div>
-            <div className={styles.header}>
-              <h1>
+            <Image className={styles.portrait} src={message.page.icon.src} alt="ryo tomita" width={300} height={450} priority />
+          </div>
+          <div>
+            <div className={styles.title}>
+              <h1 className={styles.sitename}>
                 <span className={styles.nameTitle}>行政書士</span>
                 <span className={styles.name}>冨田 良</span>
+                <span className={styles.nameYomi}>とみた りょう</span>
               </h1>
             </div>
             <div className={styles.body}>
-              <Blocks blocks={home} />
-            </div>
-            <div className={styles.body}>
-              <Blocks blocks={manifesto} />
+              <Blocks blocks={message.blocks} />
             </div>
           </div>
         </div>
 
+        <div className={styles.box}>
+          <section className={styles.evaluation}>
+            <div>
+              <h3>{collaboration.name}</h3>
+              <p><span className={styles.evaluationunit}>{collaboration.number}</span>{collaboration.unit}</p>
+            </div>
+            <div>
+              <h3>{reward.name}</h3>
+              <p><span className={styles.evaluationunit}>{reward.number}</span>{reward.unit}</p>
+            </div>
+          </section>
+        </div>
+
+        <div className={styles.box}>
+          <section className={styles.vision}>
+            <h1>{vision.title}</h1>
+            <Blocks blocks={vision.blocks} />
+          </section>
+        </div>
+
+        <div className={styles.box}>
+          <section className={styles.manifesto}>
+            <h1>{manifesto.title}</h1>
+            <Blocks blocks={manifesto.blocks} />
+          </section>
+        </div>
+
         <figure className={styles.fukuokacity}>
-          <Image className={styles.logo} src={page.cover.src} alt="Fukuoka City" fill style={{ objectFit: 'cover', objectPosition: '0 80%' }} />
+          <Image className={styles.logo} src={message.page.cover.src} alt="Fukuoka City" fill style={{ objectFit: 'cover', objectPosition: '0 80%' }} />
           <figcaption className={styles.caption}>
             写真提供：福岡市
           </figcaption>
         </figure>
 
-        <div className={styles.archivement}>
-          <Blocks blocks={archivement} />
+        <div className={styles.box}>
+          <section className={`${styles.archivement} notionate-override-half-image`}>
+            <h1>{archivement.title}</h1>
+            <Blocks blocks={archivement.blocks} />
+          </section>
         </div>
       </main>
 
-      <footer className={noto.className}>
-        <div className={styles.footer}>
+      <div className={styles.profileBox}>
+        <section className={`${styles.profile} notionate-override-profile`}>
+          <h1>{profile.title}</h1>
+          <Blocks blocks={profile.blocks} />
+        </section>
+      </div>
+
+      <footer className={styles.footer}>
           <span className={styles.copyright}>
             &copy; Ryo Tomita. {` `}
           </span>
           Powered by Next.js, Notionate, Notion, GitHub, Cloudflare and {` `}
           <a href="https://lolipop.jp/" target="_blank" rel="noopener noreferrer">Lolipop</a>.
-        </div>
       </footer>
     </>
   )
